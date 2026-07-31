0d6ea750054e2de02816ea7860e025135042e708
yvdi-abc <3625607718@qq.com>
2026-07-31 21:31:23 +0800

    chore: Configure for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/personal-blog-portfolio",
-  assetPrefix: "/personal-blog-portfolio/",
   images: { unoptimized: true },
 };
 
